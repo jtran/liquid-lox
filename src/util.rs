87892f6ab2887205ed2b648869c4473d906caa73
Jonathan Tran <jonnytran@gmail.com>
2019-10-31 23:58:59 -0400

    Change to print errors to STDERR

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 use crate::source_loc::*;
 
 pub fn error(source_loc: &SourceLoc, message: &str) {
-    println!("line {}:{}: Error: {}", source_loc.line, source_loc.column, message);
+    eprintln!("line {}:{}: Error: {}", source_loc.line, source_loc.column, message);
 }
